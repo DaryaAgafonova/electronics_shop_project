627d706e5a73ea5efac0e64187746e23e1cc8b98
daryaagafonova <ADashyta2013@yandex.ru>
2023-08-10 16:03:40 +0300

    Added new test for functions test_getter_name and test_setter_name. And fixture item1.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,27 +1,46 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
 from src.item import Item
 
+@pytest.fixture
+def item1():
 
-item1 = Item("Смартфон", 10000, 20)
+    """ Экземпляр класса в фикстуре """
+
+    return Item("Смартфон", 10000, 20)
 
 
-def test_1():
+def test_1(item1):
 
     assert item1.name == "Смартфон"
     assert item1.price == 10000
     assert item1.quantity == 20
 
 
-def test_calculate_total_price():
+def test_calculate_total_price(item1):
 
     assert item1.calculate_total_price() == 200000
 
 
-def test_apply_discount():
+def test_apply_discount(item1):
 
     Item.pay_rate = 0.8
 
     assert item1.apply_discount() == 8000.0
+
+
+def test_getter_name(item1):
+
+    assert item1.name == "Смартфон"
+
+
+def test_setter_name(item1):
+
+    item1.name = "IPhone 13 Pro"
+    assert item1.name == "IPhone 13 P"
+
+    item1.name = "IPhone 13"
+    assert item1.name == "IPhone 13"
 
 
 def test_string_to_number():
